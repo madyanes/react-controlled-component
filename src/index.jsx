4b6d2a4976266138f73edc8f0725822a63b95002
madyanes <madyanes.dev@gmail.com>
2023-07-27 02:57:56 +0700

    it becomes controlled component "single source of truth"

--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,59 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 
 class MyForm extends React.Component {
+    constructor(props) {
+        super(props)
+
+        // inisialisasi state
+        this.state = {
+            name: '',
+            email: '',
+            gender: 'Man',
+        }
+
+        // binding this context to event handler
+        this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this)
+        this.onEmailChangeEventHandler = this.onEmailChangeEventHandler.bind(this)
+        this.onGenderChangeEventHandler = this.onGenderChangeEventHandler.bind(this)
+    }
+
+    onNameChangeEventHandler(event) {
+        this.setState(() => {
+            return {
+                name: event.target.value
+            }
+        })
+    }
+
+    onEmailChangeEventHandler(event) {
+        this.setState(() => {
+            return {
+                email: event.target.value
+            }
+        })
+    }
+
+    onGenderChangeEventHandler(event) {
+        this.setState((prevState) => {
+            return {
+                gender: event.target.value
+            }
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>Registration Form</h1>
                 <form action="">
                     <label htmlFor="name">Name: </label>
-                    <input type="text" id="name" />
+                    <input type="text" id="name" value={this.state.name} onChange={this.onNameChangeEventHandler} />
                     <br />
                     <label htmlFor="email">Email: </label>
-                    <input type="text" id="email" />
+                    <input type="text" id="email" value={this.state.email} onChange={this.onEmailChangeEventHandler} />
                     <br />
                     <label htmlFor="gender">Gender: </label>
-                    <select name="" id="gender">
+                    <select name="" id="gender" value={this.state.gender} onChange={this.onGenderChangeEventHandler}>
                         <option value="Man">Man</option>
                         <option value="Woman">Woman</option>
                     </select>
